b63ad8b956f887e430478fdd77ae75a25d61a37e
Gianluca <g.camarca@tacocrypto.io>
2025-09-01 16:46:43 +0200

    movement character

--- Assets/Scripts/Character.cs
+++ Assets/Scripts/Character.cs
@@ -3,20 +3,31 @@ using UnityEngine;
 public class Character : MonoBehaviour
 {
     CharacterData _characterData;
-    MovementComponent _movementComponent = new();
-    private void Awake()
-    {
-        
-    }
+
+    InputSystem_Actions _playerInput;
+
+    MovementComponent _movementComponent;
+
+    [SerializeField] CharacterController _controller;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        _playerInput = new();
+        _playerInput.Enable();
+        _movementComponent = new(1f);
     }
 
     // Update is called once per frame
     void Update()
     {
-        _movementComponent.HandleMovement();
+        HandleMovement();
+    }
+
+    void HandleMovement()
+    {
+        Vector2 input = _playerInput.Player.Move.ReadValue<Vector2>();
+        Vector3 movement = _movementComponent.GetMovementVector(input, Time.deltaTime);
+        _controller.Move(movement);
     }
 }
