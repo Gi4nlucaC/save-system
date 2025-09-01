828369dd44da953d500959b9554632a9dcb1a286
Gianluca <g.camarca@tacocrypto.io>
2025-09-01 17:00:05 +0200

    walking idle animation + locomotion

--- Assets/Scripts/Character.cs
+++ Assets/Scripts/Character.cs
@@ -1,4 +1,4 @@
-using UnityEngine;
+﻿using UnityEngine;
 
 public class Character : MonoBehaviour
 {
@@ -9,6 +9,7 @@ public class Character : MonoBehaviour
     MovementComponent _movementComponent;
 
     [SerializeField] CharacterController _controller;
+    [SerializeField] Animator _anim;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -28,6 +29,38 @@ public class Character : MonoBehaviour
     {
         Vector2 input = _playerInput.Player.Move.ReadValue<Vector2>();
         Vector3 movement = _movementComponent.GetMovementVector(input, Time.deltaTime);
-        _controller.Move(movement);
+
+        if (movement.magnitude > 0.001f)
+        {
+            _controller.Move(movement);
+
+            Quaternion targetRotation = Get8DirRotation(input);
+            transform.rotation = targetRotation;
+            PlayAnimation("Walking");
+        }
+        else
+        {
+            PlayAnimation("Idle");
+        }
+
+    }
+
+    private Quaternion Get8DirRotation(Vector2 input)
+    {
+        if (input.sqrMagnitude < 0.01f)
+            return transform.rotation; // niente input → non ruotare
+
+        // angolo in radianti → gradi
+        float angle = Mathf.Atan2(input.x, input.y) * Mathf.Rad2Deg;
+
+        // quantizza a multipli di 45°
+        float snapped = Mathf.Round(angle / 45f) * 45f;
+
+        return Quaternion.Euler(0, snapped, 0);
+    }
+
+    void PlayAnimation(string anim)
+    {
+        _anim.Play(anim);
     }
 }
